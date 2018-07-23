5eed30fe4813051d64310ed33f042f48e69b400c
Arek Rathinavelu <arangates@gmail.com>
2018-07-23 10:52:18 +0200

    day 03

--- juicorn/polls/views.py
+++ juicorn/polls/views.py
@@ -1,5 +1,24 @@
+from html import escape
+
 from django.http import HttpResponse
+
+from polls.models import Question
 
 
 def index(request):
-    return HttpResponse("Hello polls")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse(question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+def madd(request, wasrt):
+    return HttpResponse("fuckefdejkef %s." % wasrt)
